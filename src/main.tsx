7861328a1f1f2549ca51a90624a7b8d59c655daf
Nino Filiu <nino.filiu@gmail.com>
2023-09-23 13:10:36 +0200

    three + r3f setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode />
+  <React.StrictMode>
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[10, 10, 10, 10, 10, 10]} />
+        <meshBasicMaterial wireframe />
+      </mesh>
+    </Canvas>
+  </React.StrictMode>
 );
