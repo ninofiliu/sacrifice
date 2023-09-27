d20ea37c961ec36ce5b54e43b7f01f5413ab1477
Nino Filiu <nino.filiu@gmail.com>
2023-09-27 15:41:23 +0200

    adam runnin

--- src/World.tsx
+++ src/World.tsx
@@ -1,32 +1,79 @@
 import { OrbitControls, useAnimations, useGLTF } from "@react-three/drei";
 import { useEffect, useRef } from "react";
-import type { SkinnedMesh } from "three";
+import type { Group, Material, Object3DEventMap, SkinnedMesh } from "three";
 import type { GLTF } from "three-stdlib";
 
-export const World = () => {
-  const group = useRef();
-  const { nodes, animations } = useGLTF("/mine.glb") as GLTF & {
+useGLTF.preload("/AdamRunning.glb");
+
+export const AdamRunning = () => {
+  const group = useRef<Group<Object3DEventMap>>(null!);
+  const { nodes, materials, animations } = useGLTF(
+    "/AdamRunning.glb"
+  ) as GLTF & {
     nodes: Record<string, SkinnedMesh>;
+    materials: Record<string, Material>;
   };
   const { actions } = useAnimations(animations, group);
   useEffect(() => {
     actions["Armature|mixamo.com|Layer0"]?.play();
   }, [actions]);
   return (
-    <>
-      <group ref={group} dispose={null}>
-        <group name="Scene">
-          <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
-            <primitive object={nodes.mixamorigHips} />
-            <skinnedMesh
-              name="Cube"
-              geometry={nodes.Cube.geometry}
-              material={nodes.Cube.material}
-              skeleton={nodes.Cube.skeleton}
-            />
-          </group>
+    <group ref={group} dispose={null}>
+      <group name="Scene">
+        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <primitive object={nodes.mixamorig7Hips} />
+          <skinnedMesh
+            name="Ch08_Beard"
+            geometry={nodes.Ch08_Beard.geometry}
+            material={materials.Ch08_hair}
+            skeleton={nodes.Ch08_Beard.skeleton}
+          />
+          <skinnedMesh
+            name="Ch08_Body"
+            geometry={nodes.Ch08_Body.geometry}
+            material={materials.Ch08_body}
+            skeleton={nodes.Ch08_Body.skeleton}
+          />
+          <skinnedMesh
+            name="Ch08_Eyelashes"
+            geometry={nodes.Ch08_Eyelashes.geometry}
+            material={materials.Ch08_hair}
+            skeleton={nodes.Ch08_Eyelashes.skeleton}
+          />
+          <skinnedMesh
+            name="Ch08_Hair"
+            geometry={nodes.Ch08_Hair.geometry}
+            material={materials.Ch08_hair}
+            skeleton={nodes.Ch08_Hair.skeleton}
+          />
+          <skinnedMesh
+            name="Ch08_Hoodie"
+            geometry={nodes.Ch08_Hoodie.geometry}
+            material={materials.Ch08_body1}
+            skeleton={nodes.Ch08_Hoodie.skeleton}
+          />
+          <skinnedMesh
+            name="Ch08_Pants"
+            geometry={nodes.Ch08_Pants.geometry}
+            material={materials.Ch08_body1}
+            skeleton={nodes.Ch08_Pants.skeleton}
+          />
+          <skinnedMesh
+            name="Ch08_Sneakers"
+            geometry={nodes.Ch08_Sneakers.geometry}
+            material={materials.Ch08_body1}
+            skeleton={nodes.Ch08_Sneakers.skeleton}
+          />
         </group>
       </group>
+    </group>
+  );
+};
+
+export const World = () => {
+  return (
+    <>
+      <AdamRunning />
       <OrbitControls />
       <ambientLight />
       <axesHelper />
