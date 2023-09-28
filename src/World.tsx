c0962698903596b4fa0eee8efd13f3d628f7b18f
Nino Filiu <nino.filiu@gmail.com>
2023-09-28 02:01:12 +0200

    running and looping path

--- src/World.tsx
+++ src/World.tsx
@@ -1,25 +1,43 @@
 import { Environment, OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import type { ReactNode } from "react";
+import { useState } from "react";
 
 import { AdamRunning } from "./AdamRunning";
 
-const Rocks = () =>
-  Array(10)
-    .fill(null)
-    .map((_, i) => (
-      <mesh key={i} position={[0, 0, 4 * i + 2]}>
-        <boxGeometry args={[1, 1, 1, 1, 1, 1]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
-    ));
+const PATH_SPEED = 6;
+const PATH_LENGTH = 30;
+
+const Path = ({ children }: { children: ReactNode }) => {
+  const [z, setZ] = useState(0);
+  useFrame((_, delta) => {
+    setZ((z + delta * PATH_SPEED) % PATH_LENGTH);
+  });
+  return (
+    <>
+      <group position={[0, 0, -z - PATH_LENGTH]}>{children}</group>
+      <group position={[0, 0, -z]}>{children}</group>
+      <group position={[0, 0, -z + PATH_LENGTH]}>{children}</group>
+    </>
+  );
+};
 
 export const World = () => {
   return (
     <>
       <AdamRunning />
-      <Rocks />
+      <Path>
+        <mesh
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, 0, PATH_LENGTH / 2]}
+        >
+          <planeGeometry args={[1, PATH_LENGTH, 1, 1]} />
+          <meshBasicMaterial wireframe />
+        </mesh>
+      </Path>
 
       <ambientLight />
-      <fog attach="fog" color="grey" near={1} far={35} />
+      <fog attach="fog" color="grey" near={1} far={25} />
       <Environment preset="forest" />
       <color attach="background" args={["grey"]} />
 
