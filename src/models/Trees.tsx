59369be64705457e8414f328366e51871b2ab0a8
Nino Filiu <nino.filiu@gmail.com>
2023-10-19 23:21:07 +0200

    fps=17, t=58ms: use OaksLight

--- src/models/Trees.tsx
+++ src/models/Trees.tsx
@@ -7,10 +7,10 @@ import { FOG, TREADMILL_SPEED } from "../consts";
 import { useSwitches, useTime } from "../ddj";
 import { mod, rf, rp } from "../shorts";
 
-useGLTF.preload("/oaks.glb");
+useGLTF.preload("/OaksLight.glb");
 
-export const Oak1 = () => {
-  const { nodes, materials } = useGLTF("/oaks.glb") as GLTF & {
+const Oak1 = () => {
+  const { nodes, materials } = useGLTF("/OaksLight.glb") as GLTF & {
     nodes: Record<string, SkinnedMesh>;
     materials: Record<string, Material>;
   };
@@ -37,49 +37,7 @@ export const Oak1 = () => {
   );
 };
 
-export const Oak2 = () => {
-  const { nodes, materials } = useGLTF("/oaks.glb") as GLTF & {
-    nodes: Record<string, SkinnedMesh>;
-    materials: Record<string, Material>;
-  };
-  return (
-    <group dispose={null} scale={0.1}>
-      <mesh
-        geometry={nodes.structure001.geometry}
-        material={materials.bark}
-        rotation={[Math.PI / 2, 0, 0]}
-      />
-      <mesh
-        geometry={nodes.foliage001.geometry}
-        material={materials.foliage}
-        rotation={[Math.PI / 2, 0, 0]}
-      />
-    </group>
-  );
-};
-
-export const Oak3 = () => {
-  const { nodes, materials } = useGLTF("/oaks.glb") as GLTF & {
-    nodes: Record<string, SkinnedMesh>;
-    materials: Record<string, Material>;
-  };
-  return (
-    <group dispose={null} scale={0.1}>
-      <mesh
-        geometry={nodes.structure002.geometry}
-        material={materials.bark}
-        rotation={[Math.PI / 2, 0, 0]}
-      />
-      <mesh
-        geometry={nodes.foliage002.geometry}
-        material={materials.foliage}
-        rotation={[Math.PI / 2, 0, 0]}
-      />
-    </group>
-  );
-};
-
-export const Tree = ({ xMin, xMax }: { xMin: number; xMax: number }) => {
+const Tree = ({ xMin, xMax }: { xMin: number; xMax: number }) => {
   const x = useRef(rf(xMin, xMax) * rp([-1, 1]));
   const ry = useRef(rf(0, Math.PI));
   const z0 = useRef(rf(0, 2 * FOG));
